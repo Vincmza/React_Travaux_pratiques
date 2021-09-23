f1ff0e3c9411e2aa2ba6e0452a68c936f5602b13
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-09-23 17:22:46 +0200

    App.js and Student.js: set up new features

--- react-formation-ultime/src/components/students/Student.js
+++ react-formation-ultime/src/components/students/Student.js
@@ -1,23 +1,42 @@
 //Libraries
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import './Student.css';
 
-//props est inhérent à Component qui met à disposition un
-//objet des différentes propriétés déclarées dans App.js
-//la propriété children fait directement référence à la citation 
-//dans la déclaration du composant Student dans App.js
+function Student(props){
 
-class Student extends Component {
-    render(){
-        return (
-            <div className="student">
-                <h1>{this.props.name}</h1>
-                <p>Moyenne Scolaire : {this.props.moyenne}/20</p>
-                <p>Age : {Math.floor(Math.random()*100)}</p>
-                <i>{this.props.children}</i> 
-            </div>
-        );
-    }
+    //componentDidMount
+    useEffect(()=>{
+        console.log('[Student.js] componentDidMount')
+    },[]);
+
+    //componentDidUpdate
+    useEffect(()=>{
+        console.log('[Student.js] componentDidUpdate')
+    },[]);
+
+    //componentWillUnmount
+    useEffect(()=>{
+        return()=>{
+            console.log('[Student.js] componentWillUnmount')
+        }
+    },[]);
+
+    //Limiteur
+    useEffect(()=>{
+        return()=>{
+            console.log('[Student.js] le nom a bien été modifié')
+        }    
+    },[props.name]);
+
+
+    return (
+        <div className="student">
+            <h1 onClick={props.clic}>{props.name}</h1>
+            <p>Moyenne Scolaire : {props.moyenne}/20</p>
+            <p>Age : {Math.floor(Math.random()*100)}</p>
+            <i>{props.children}</i> 
+        </div>
+    );
 }
 
 export default Student;
--- react-formation-ultime/src/container/app/App.js
+++ react-formation-ultime/src/container/app/App.js
@@ -1,70 +1,70 @@
 //Libraries
 import './App.css';
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 
 //Components
 import Student from '../../components/students/Student';
 
-class App extends Component {
+function App(){
 
-//State est un objet dans lequel on stocke des données pour les injecter dynamiquement
+//State
 
-  state = {
-    students : [
-      {
-        nom: 'Eva Dupont', 
-        moyenne: 15, 
-        citation: "J'aime les salsifis"
-      },
-      {
-        nom: 'Jacques Chirac', 
-        moyenne: 11,
-        citation: null
-      },
-    ],
-    maintenance : false
-  }
+  const [students, setStudents]=useState([
+    {
+      nom: 'Eva Dupont', 
+      moyenne: 15, 
+      citation: "J'aime les salsifis"
+    },
+    {
+      nom: 'Jacques Chirac', 
+      moyenne: 11,
+      citation: null
+    },
+    {maintenance: false}
+  ]);
 
-//buttonClicked est un fonction fléchée executant du code permettant
-//de modifier le nom du premier élève au clic sur le bouton associé
-//l'attribut onClick plus bas dans la balise <button> est 
-//l'évènement associé au bouton affiché sur l'interface visuel
+//Etats
 
-  buttonClicked = ()=>{
-    const newState = [...this.state.students]; //copie de l'objet state grâce au Rest operator dans une constante
-    newState[0].nom = 'Agnès Buzyn' //modification des données, ici le nom de l'index 0 du tableau students
-    this.setState({ //mise à jour de l'objet state avec la méthode setState
-      ...this.state, //1er paramètre = copie de l'objet state
-      students: newState //2eme paramètre = la modification enregistrée dans la variable newState
-    });
-  }
-  
-//render nous permet de retourner nos composants à l'image de l'instruction Return au sein
-//d'une fonction classique ou fléchée
+  useEffect(()=>{
+    
+  });
 
-  render(){
-    return (
-      <div className="App">
-        <h1>Bienvenue dans la classe Terre !</h1>
+//Fonctions
 
-        {/*onClick est l'attribut dans la balise bouton qui déclenche l'execution du code 
-        contenu dans la fonction fléchée buttonClicked*/}
+  const buttonClicked = newName =>{
+    const newStudent = [...students];
+    newStudent[0].nom = newName;
+    setStudents(newStudent); 
+    };
 
-        <button onClick={this.buttonClicked}>Renommer le premier élève</button>
+//JSX
 
-        <Student 
-        name={this.state.students[0].nom} 
-        moyenne={this.state.students[0].moyenne}>
-          {this.state.students[0].citation} 
-        </Student>
+  return (
+    <div className="App">
+      <h1>Bienvenue dans la classe Terre !</h1>
 
-        <Student 
-        name={this.state.students[1].nom} 
-        moyenne={this.state.students[1].moyenne}>
-        </Student>
-      </div>
-    );
-  }
+      {/*onClick est l'attribut dans la balise bouton qui déclenche l'execution du code 
+      contenu dans la fonction fléchée buttonClicked*/}
+
+      <button onClick={buttonClicked.bind(this, "Jeanne d'Arc")}>Renommer le premier élève</button>
+
+      <Student 
+        name={students[0].nom} 
+        moyenne={students[0].moyenne}
+        clic={()=>buttonClicked('Ken le survivant')}
+      >
+        {students[0].citation}
+        
+      </Student>
+
+      <Student 
+        name={students[1].nom} 
+        moyenne={students[1].moyenne}
+        clic={()=>buttonClicked('Jean Marie Le Pen')}
+      >
+      </Student>
+    </div>
+  );
 }
 //Exportation de la class App dans notre index.js
 //Le fichier index.js injecte ensuite les données dans le fichier HTML
